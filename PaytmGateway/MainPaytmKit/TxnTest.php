28d4197cb76f5143e343acf0731cbf30cd6d368b
GOURBOY <abhaygour20253@acropolis.in>
2022-11-11 00:07:51 +0530

    CommitFinalaw

--- PaytmGateway/MainPaytmKit/TxnTest.php
+++ PaytmGateway/MainPaytmKit/TxnTest.php
@@ -1,4 +1,5 @@
 <?php
+
 	header("Pragma: no-cache");
 	header("Cache-Control: no-cache");
 	header("Expires: 0");
@@ -9,13 +10,33 @@
 <head>
 <title>Merchant Check Out Page</title>
 <meta name="GENERATOR" content="Evrsoft First Page">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+<style>
+	.img1{
+		filter: blur(4px);	 
+		display:absolute;
+			
+	}
+	.dis{
+		position:absolute;
+		height:350px;
+		width:500px;
+		top:200px;
+		left: 500px;
+		background-color: rgba(232, 223, 95, 0.749);
+		
+	}
+</style>
 </head>
 <body>
-	<h1>Merchant Check Out Page</h1>
-	<pre>
-	</pre>
+<?php require 'C:\xampp\htdocs\Projects\AcroProj\FINAL_PROJ\Connect\partials\_nav.php';?>
+
+	<div>
+	<img  class="img1" src="Images/mus1.jpg" width="1520px" height="650px">
+
+	<div class="dis"><h1><center>Merchant Check Out Page</center></h1>
 	<form method="post" action="pgRedirect.php">
-		<table border="1">
+	<center><table border="12">
 			<tbody>
 				<tr>
 					<th>S.No</th>
@@ -24,7 +45,7 @@
 				</tr>
 				<tr>
 					<td>1</td>
-					<td><label>ORDER_ID::*</label></td>
+					<td><label>ORDER_ID* :</label></td>
 					<td><input id="ORDER_ID" tabindex="1" maxlength="20" size="20"
 						name="ORDER_ID" autocomplete="off"
 						value="<?php echo  "ORDS" . rand(10000,99999999)?>">
@@ -32,24 +53,24 @@
 				</tr>
 				<tr>
 					<td>2</td>
-					<td><label>CUSTID ::*</label></td>
-					<td><input id="CUST_ID" tabindex="2" maxlength="12" size="12" name="CUST_ID" autocomplete="off" value="CUST001"></td>
+					<td><label>CUSTID* :</label></td>
+					<td><input id="CUST_ID" tabindex="2" maxlength="12" size="20" name="CUST_ID" autocomplete="off" value="CUST001"></td>
 				</tr>
 				<tr>
 					<td>3</td>
-					<td><label>INDUSTRY_TYPE_ID ::*</label></td>
-					<td><input id="INDUSTRY_TYPE_ID" tabindex="4" maxlength="12" size="12" name="INDUSTRY_TYPE_ID" autocomplete="off" value="Retail"></td>
+					<td><label>INDUSTRY_TYPE_ID* :</label></td>
+					<td><input id="INDUSTRY_TYPE_ID" tabindex="4" maxlength="12" size="20" name="INDUSTRY_TYPE_ID" autocomplete="off" value="Retail"></td>
 				</tr>
 				<tr>
 					<td>4</td>
-					<td><label>Channel ::*</label></td>
+					<td><label>Channel* :</label></td>
 					<td><input id="CHANNEL_ID" tabindex="4" maxlength="12"
-						size="12" name="CHANNEL_ID" autocomplete="off" value="WEB">
+						size="20" name="CHANNEL_ID" autocomplete="off" value="WEB">
 					</td>
 				</tr>
 				<tr>
 					<td>5</td>
-					<td><label>txnAmount*</label></td>
+					<td><label>txnAmount* :</label></td>
 					<td><input title="TXN_AMOUNT" tabindex="10"
 						type="text" name="TXN_AMOUNT"
 						value="1000">
@@ -58,11 +79,14 @@
 				<tr>
 					<td></td>
 					<td></td>
-					<td><input value="CheckOut" type="submit"	onclick=""></td>
+					<td><input value="CheckOut" type="submit" style="width:189px;"	onclick=""></td>
 				</tr>
 			</tbody>
 		</table>
-		* - Mandatory Fields
-	</form>
+		
+	</form>  </div>
+	
+</div>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </body>
 </html>
